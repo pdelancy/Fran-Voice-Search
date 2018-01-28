8a2aa72bed7f426c78f241c55f1ec54884cee045
pdelancy <pdelancy94@gmail.com>
2018-01-28 11:18:25 -0800

    final version

--- routes/twilio.js
+++ routes/twilio.js
@@ -8,6 +8,8 @@ var apiai = require('apiai-promise');
 var app = apiai(process.env.API_AI_TOKEN);
 const axios = require('axios');
 const { Place } = require('../models/models');
+const intents = ["Food", "Shelter", "Medical", "Rehab", "Shower"];
+let phoneNumber = "";
 
 router.post('/receiveMessage', (req, res)=>{
   console.log('inside receive Message');
@@ -17,16 +19,20 @@ router.post('/receiveMessage', (req, res)=>{
   app.textRequest(input, {
     sessionId: req.body.From
   }).then((response2)=>{
-    if(!response2.result.actionIncomplete){
+    if(!response2.result.actionIncomplete  && intents.indexOf(response2.result.metadata.intentName) !== -1){
       query(response2)
       .then((place) => {
         console.log("*************\n", place);
-        response.sms(`I found ${place.name} at ${place.location}`);
-        const textString = place.direction.join("\n")
-        console.log(textString);
-        place.direction.map((instruction) => {
-          response.sms(instruction);
-        })
+        if(!place){
+          response.sms("I am sorry, there is no available service at this time.")
+        } else {
+          response.sms(`I found ${place.name} at ${place.location}`);
+          const textString = place.direction.join("\n")
+          console.log(textString);
+          place.direction.map((instruction) => {
+            response.sms(instruction);
+          });
+        }
         res.writeHead(200, { 'Content-Type': 'text/xml' });
         res.end(response.toString());
       })
@@ -49,8 +55,18 @@ router.post('/receiveCall', (req, res) => {
   const response = new VoiceResponse();
   let completed = false;
   let newResponse;
-
-  if(req.body.SpeechResult){
+  console.log(req.body);
+  if( req.body.Digits ){
+    console.log(req.body.Digits);
+    if(req.body.Digits === '1'){
+      response.dial(phoneNumber);
+    } else if(req.body.Digits === '2'){
+      response.dial("301-873-0062");
+    }
+    phoneNumber = "";
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(response.toString());
+  } else if(req.body.SpeechResult){
     let input = req.body.SpeechResult;
     // console.log(input);
     let dialogFlowResponded = false;
@@ -66,7 +82,7 @@ router.post('/receiveCall', (req, res) => {
       let completed = !response2.result.actionIncomplete;
       dialogFlowResponded = true;
       response.say( JSON.stringify(newResponse));
-      if(!completed){
+      if(!completed || intents.indexOf(response2.result.metadata.intentName) === -1){
         const gather = response.gather({
           input: 'speech dtmf',
           timeout: 5
@@ -76,16 +92,31 @@ router.post('/receiveCall', (req, res) => {
         });
         return;
       } else {
-        console.log(response2.result);
+        // console.log(response2.result);
         return query(response2)
         .then((place) => {
-          console.log("*************\n", place);
-          response.say(`I found ${place.name} at ${place.location}`);
-          const textString = place.direction.join("\n")
-          console.log(textString);
-          place.direction.map((instruction) => {
-            response.sms(instruction);
-          })
+          // console.log("*************\n", place);
+          if(!place){
+            response.say("I am sorry, the service you are looking for is probably not yet in our database. It will be soon");
+          } else {
+            response.say(`I found ${place.name} at ${place.location}`);
+            const textString = place.direction.join("\n")
+            // console.log(textString);
+            place.direction.map((instruction) => {
+              response.sms(instruction);
+            })
+            if(place.phoneNumber){
+              phoneNumber = place.phoneNumber;
+              response.say(`This service has a hotline, would you like me to connect you? To connect, press 1. To call Paul, press 2. To end the call, press 3.`);
+              const gather = response.gather({
+                input: 'dtmf',
+                timeout: 5,
+                // action: '/readText',
+                // method: 'GET'
+                numDigits: 1,
+              });
+            }
+          }
           // response.dial('614-531-7501');
           return;
         })
@@ -113,11 +144,10 @@ router.post('/receiveCall', (req, res) => {
   }
 })
 
-const intents = ["Food", "Shelter", "Medical", "Rehab", "Shower"];
 let timeAvailable = (places, dt) => {
-    console.log("line 11", dt);
+    // console.log("line 11", dt);
     let day = dt.getDay();
-    console.log(96, day);
+    // console.log(96, day);
     let dth = (dt.getHours() < 10) ? "0" + JSON.stringify(dt.getHours()) : JSON.stringify(dt.getHours());
     let dtm = (dt.getMinutes() < 10) ? "0" + JSON.stringify(dt.getMinutes()) : JSON.stringify(dt.getMinutes());
     let desiredTime = new Date();
@@ -154,14 +184,14 @@ let timeAvailable = (places, dt) => {
           return validTime;
         }
     }).filter( place => place );
-    console.log("Line 121", things);
+    //console.log("Line 121", things);
     return things
 };
 const query = (body) => {
-    console.log("line 28");
+    //console.log("line 28");
     let response = "Hi Luchenn and Paul";
     let params = body.result.parameters;
-    console.log(body);
+    //console.log(body);
     let type;
     let dateTime;
     let location;
@@ -171,44 +201,45 @@ const query = (body) => {
     // else {
     //     location = (params.location["street-address"]) ? params.location["street-address"] : Object.values(params.location).join(", ");
     // }
-    console.log(location);
+    //console.log(location);
     intents.map(i => {
         if(body.result.metadata.intentName === i){
             type = i;
         }
     });
-    console.log(type);
-    console.log(params.date);
+    //console.log(type);
+    //console.log(params.date);
     dateTime = (type === "Rehab" || type === "Shelter") ? new Date(params.date) : new Date(params.date + "T" + params.time);
-    console.log("line 40", dateTime);
+    //console.log("line 40", dateTime);
+    // if(!location) return {smallTalk: true};
     return Place.find({
         type: type
     }).then(places => {
-        console.log("line 44 in Place", places);
+        //console.log("line 44 in Place", places);
         let thePlace = timeAvailable(places, dateTime);
-        console.log("the Place", thePlace);
+        //console.log("the Place", thePlace);
         let fromLocation = location + ", SanFrancisco, CA";
         // fromLocation = fromLocation.split(" ").join("+");
         return Promise.all(thePlace.map(tp => {
-            console.log("line 48 in promise all");
-            console.log(tp);
+            //console.log("line 48 in promise all");
+            //console.log(tp);
             let toLocation = tp.location.split(" ").join("+");
-            console.log(toLocation);
+            //console.log(toLocation);
             return axios.get(`http://www.mapquestapi.com/directions/v2/route?key=${process.env.MAPQUEST_KEY}` +
                     `&from=${fromLocation}&to=${toLocation}`)
                     .then(directions => {
-                      console.log(directions.data.route);
+                      //console.log(directions.data.route);
                         let dir = {
                             distance: parseFloat(directions.data.route.distance),
                             direction: directions.data.route.legs[0].maneuvers.map(maneuver => {
-                              console.log(maneuver);
+                              //console.log(maneuver);
                                 return maneuver.narrative;
                             }),
                             name: tp.name,
                             phoneNumber: tp.phoneNumber,
                             location: tp.location
                         };
-                        console.log(dir);
+                        //console.log(dir);
                         return dir;
                     })
                     .catch(e => {
@@ -216,8 +247,8 @@ const query = (body) => {
                     });
             }))
             .then((r) => {
-                console.log("line 69 .then of Promise");
-                console.log(r);
+                //console.log("line 69 .then of Promise");
+                //console.log(r);
                 return r.reduce((short, disdir) => {
                     if(short === null || disdir.distance < short.distance ) {
                         return disdir;
@@ -227,7 +258,7 @@ const query = (body) => {
                 }, null);
             })
             .then(resp => {
-                console.log("line 78",resp);
+                //console.log("line 78",resp);
                 return resp;
             })
             .catch(e => {
