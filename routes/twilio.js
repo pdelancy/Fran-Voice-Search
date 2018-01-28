f398550bccd97ec26359908d259e1783d52f9935
pdelancy <pdelancy94@gmail.com>
2018-01-27 19:43:04 -0800

    talking

--- routes/twilio.js
+++ routes/twilio.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
-var apiai = require('apiai');
+var apiai = require('apiai-promise');
 var app = apiai(process.env.API_AI_TOKEN);
 
 router.post('/receiveCall', (req, res) => {
@@ -14,40 +14,41 @@ router.post('/receiveCall', (req, res) => {
 
   if(req.body.SpeechResult){
     let input = req.body.SpeechResult;
+    console.log(input);
     let dialogFlowResponded = false;
     //call dialogflow
-    var request = app.textRequest(input, {
-        sessionId: sessionId
-    });
-
-    request.on('response', function(response) {
-        console.log(response);
-        let newResponse = response.result.fulfillment.speech;
-        let completed = !response.result.actionIncomplete;
+    console.log("in conversation");
+    console.log(req.body.From);
+    app.textRequest(input, {
+        sessionId: req.body.From
+    }).then((response2) => {
+      let newResponse = response2.result.fulfillment.speech;
+      console.log(response);
+      console.log(newResponse);
+      let completed = !response2.result.actionIncomplete;
+      dialogFlowResponded = true;
+      response.say(JSON.stringify(newResponse));
+      if(!completed){
+        const gather = response.gather({
+          input: 'speech dtmf',
+          timeout: 3
+          // action: '/readText',
+          // method: 'GET'
+          // numDigits: 1,
+        });
+        return;
+      }
+    }).then(() => {
+      res.writeHead(200, { 'Content-Type': 'text/xml' });
+      res.end(response.toString());
     })
-
-    while( !dialogFlowResponded ){
-        console.log('\n waiting...');
-        request.on('error', function(errer){
-            console.log(error);
-        })
-    }
-
-    response.say(newResponse);
-    if(!completed){
-      const gather = response.gather({
-        input: 'speech dtmf',
-        timeout: 3
-        // action: '/readText',
-        // method: 'GET'
-        // numDigits: 1,
-      });
-    }
+    .catch((err)=>{
+      console.log(err);
+    })
     // response.sms(`You said ${req.body.SpeechResult}`);
 
   } else if(!completed){
     response.say('Welcome to Fran A I! What can I help you with?');
-    console.log(response.gather);
     const gather = response.gather({
       input: 'speech dtmf',
       timeout: 3
@@ -55,10 +56,9 @@ router.post('/receiveCall', (req, res) => {
       // method: 'GET'
       // numDigits: 1,
     });
+    res.writeHead(200, { 'Content-Type': 'text/xml' });
+    res.end(response.toString());
   }
-  res.writeHead(200, { 'Content-Type': 'text/xml' });
-  res.end(response.toString());
-
 })
 
 module.exports = router;
